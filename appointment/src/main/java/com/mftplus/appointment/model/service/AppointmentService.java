72253c439700e9407a03dc97ecb3c007f27b7771
sana center <azimi1376@gmail.com>
2025-08-16 07:18:00 +0330

    post via kafka success

--- appointment/src/main/java/com/mftplus/appointment/model/service/AppointmentService.java
+++ appointment/src/main/java/com/mftplus/appointment/model/service/AppointmentService.java
@@ -64,7 +64,6 @@ public class AppointmentService {
 
         AppointmentDto resultDto = appointmentMapper.toDto(savedAppointment);
         resultDto.getPersianStartDate();
-
         return resultDto;
     }
 
